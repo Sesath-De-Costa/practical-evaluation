86aff961ca9bd7c2c1ad1154790088ef7d4f181a
sesath <sesathdecosta@gmail.com>
2021-03-26 17:52:21 +0530

    Initial commit

---
+++ src/main/java/com/rootcode/interview/practicle/PracticleApplication.java
@@ -0,0 +1,14 @@
+package com.rootcode.interview.practicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PracticleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rootcode/interview/practicle/PracticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootcode.interview.practicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
