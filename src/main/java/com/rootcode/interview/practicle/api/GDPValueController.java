992e4153d1638d2c45d06fc66a92ba2999f1c7de
sesath <sesathdecosta@gmail.com>
2021-03-26 18:08:16 +0530

    Add GDPController

---
+++ src/main/java/com/rootcode/interview/practicle/api/GDPValueController.java
@@ -0,0 +1,17 @@
+package com.rootcode.interview.practicle.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Sesath De Costa on 2021-03-26
+ * <p>
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ **/
+@CrossOrigin
+@RequestMapping("/api/v1/GDP_values")
+@RestController
+public class GDPValueController {
+}
